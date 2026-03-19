bb25c1253f138c2681e5a4e757b3780bbf8deda8
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2026-03-19 06:20:19 +0100

    Update category.go

--- models/category.go
+++ models/category.go
@@ -8,6 +8,8 @@ type Category struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Name      string    `gorm:"size:255;not null" json:"name"`
 	Slug      string    `gorm:"size:255;unique;not null" json:"slug"`
+	Image     *string   `gorm:"size:500" json:"image,omitempty"`
+	Products  []Product `gorm:"foreignKey:CategoryID" json:"products,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
